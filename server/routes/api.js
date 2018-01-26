0efda4cb4c5b7973c5f7dc847eaf28f39f07cfdf
Denislav Davidov <denizdavutoglu@gmail.com>
2018-01-26 13:04:48 +0000

    first commit

---
+++ server/routes/api.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const ObjectID = require("mongodb").ObjectID;
+const mongojs = require("mongojs");
+const db = mongojs("service_transponder", ["users"]);
+
+// Error handling
+const sendError = (err, res) => {
+ response.status = 501;
+ response.message = typeof err == "object" ? err.message : err;
+ res.status(501).json(response);
+};
+
+// Response handling
+let response = {
+ status: 200,
+ data: [],
+ message: null
+};
+
+// Get users
+router.get("/users", (req, res, next) => {
+ db.users.find((err, users) => {
+  if (err) return next(err);
+  response.data = users;
+  res.json(response);
+ });
+});
+
+module.exports = router;﻿
